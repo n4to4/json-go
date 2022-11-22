55b2814a63bef5e56a7237050dcf8b2e16bdb3db
n4to4 <n4to4k@gmail.com>
2022-11-22 18:51:05 +0900

    JSON playground

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("json-go")
+	birdJson := []string{
+		`{}`,
+		`{"birdType": "pigeon","description": "likes to perch on rocks"}`,
+		`{"name":"string","age":42,"array":[1,true,null],"empty":null}`,
+	}
+
+	for _, j := range birdJson {
+		fmt.Printf("\ninput: %q\n", j)
+
+		var bird map[string]any
+		if err := json.Unmarshal([]byte(j), &bird); err != nil {
+			fmt.Printf("error: %s", err)
+			continue
+		}
+
+		fmt.Printf("%#v\n", bird)
+		fmt.Printf("%#v\n", bird["birdType"])
+		fmt.Printf("%#v\n", bird["birdtype"])
+	}
 }
--- main_test.go
+++ main_test.go
@@ -3,5 +3,4 @@ package main
 import "testing"
 
 func TestParseObject(t *testing.T) {
-	// TODO
 }
