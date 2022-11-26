54f21333bcb160d7fd9bf2b353ae74d45e1c05b1
n4to4 <n4to4k@gmail.com>
2022-11-26 09:05:58 +0900

    wip

--- main.go
+++ main.go
@@ -29,32 +29,66 @@ func main() {
 }
 
 func Unmarshal(src string, dst *map[string]any) error {
-	if src[0] != '{' {
+	um := Unmarshaler{src: src, cur: 0, dst: dst}
+	if err := um.UnmarshalObject(); err != nil {
+		return err
+	}
+	return nil
+}
+
+type Unmarshaler struct {
+	src string
+	cur int
+	dst *map[string]any
+}
+
+func (u *Unmarshaler) currentChar() (byte, bool) {
+	if u.cur < len(u.src) {
+		return u.src[u.cur], true
+	} else {
+		return ' ', false
+	}
+}
+
+func (u *Unmarshaler) next() {
+	u.cur += 1
+}
+
+func (u *Unmarshaler) UnmarshalObject() error {
+	c, ok := u.currentChar()
+	if !ok {
+		return errors.New("unexpected EOF")
+	}
+	if c != '{' {
 		return errors.New("not supported")
 	}
+	u.next()
 
-	obj, err := parseObject(src[1:])
+	obj, err := u.parseObject()
 	if err != nil {
 		return err
 	}
-	*dst = obj
+	*u.dst = obj
 
 	return nil
 }
 
-func parseObject(src string) (map[string]any, error) {
+func (u *Unmarshaler) parseObject() (map[string]any, error) {
 	m := make(map[string]any)
 	var key string
 	haveKey := false
-	var idx int
-	for idx = 0; idx < len(src); idx++ {
-		c := src[idx]
+	for {
+		c, ok := u.currentChar()
+		if !ok {
+			return nil, errors.New("unexpected EOF")
+		}
 		switch c {
 		case '}':
 			return m, nil
 		case '"':
-			s, i, err := parseString(src[idx+1:])
-			fmt.Printf("case %q, %d\n", s, i)
+			u.next()
+			s, err := u.parseString()
+			// fmt.Printf("case %q, %d\n", s, u.cur)
 			if err != nil {
 				return nil, err
 			}
@@ -65,24 +99,28 @@ func parseObject(src string) (map[string]any, error) {
 				key = s
 				haveKey = true
 			}
-			idx += i + 1
 		case ':':
+			u.next()
 		}
 	}
-	return m, nil
 }
 
-func parseString(src string) (string, int, error) {
-	fmt.Printf("parseString: %q\n", src)
+func (u *Unmarshaler) parseString() (string, error) {
+	// fmt.Printf("parseString: %q\n", u.src)
 
 	var str string
-	for i, c := range src {
+	for {
+		c, ok := u.currentChar()
+		if !ok {
+			return "", errors.New("unexpected EOF")
+		}
 		switch c {
 		case '"':
-			return str, i, nil
+			u.next()
+			return str, nil
 		default:
+			u.next()
 			str = str + string(c)
 		}
 	}
-	return "", 0, fmt.Errorf("parseString: missing closing `\"`: %q", src)
 }
